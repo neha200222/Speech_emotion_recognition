c5c8591ff8134d2cc3c1a3315ec94663fd796b11
neha200222 <74524375+neha200222@users.noreply.github.com>
2024-06-29 19:24:08 +0530

    Create evaluate.py

---
+++ evaluate.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+def plot_history(history):
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
